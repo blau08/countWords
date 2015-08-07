daa1fff2fa1bb7c2ce9d7cc8b299333d84fa9e8a
Brian Lau <blau08@gmail.com>
2015-08-07 10:51:12 -0700

    it will count word in string

--- lib/countWords.rb
+++ lib/countWords.rb
@@ -1,16 +1,21 @@
+require('pry')
+
 class String
 
   define_method(:countWords) do |words|
     word = self
     anagrams = words.split(" ")
+    # binding.pry
     new_array = []
     count = 0
     sentence = ""
 
     anagrams.each do |anagram|
-      x = anagram.scan(/word/)
+      x = anagram
+      # binding.pry
       if x == word
         count += 1
+        # binding.pry
       end
     end
     count
