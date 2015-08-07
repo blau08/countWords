3c0c50171ca77efd424002513f5c0664c19f4b2a
Brian Lau <blau08@gmail.com>
2015-08-07 10:55:09 -0700

    it will add integration spec

---
+++ spec/countWords_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('wordCount path', {:type => :feature}) do
+  it('processes the user entry and he count of word in string') do
+    visit('/')
+    fill_in('word', :with => "dog")
+    fill_in('list', :with => "dog dog cat")
+    click_button("Count Words!")
+    expect(page).to have_content(2)
+  end
+end
